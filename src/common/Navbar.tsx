d87f5eb546f7b6ee9304c1cf104780a66653254b
jihansoft <jihanuddinsoft@gmail.com>
2025-07-10 12:05:46 +0600

    project setup

---
+++ src/common/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h3 className="text-[20px] font-bold text-background ">
+        Swan <span>Global</span>
+      </h3>
+    </nav>
+  );
+}
