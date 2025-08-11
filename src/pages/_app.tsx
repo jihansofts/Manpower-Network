88e3727648ba03e9e59f5ee4481c95832b16ef46
jihansoft <jihanuddinsoft@gmail.com>
2025-08-11 19:28:20 +0600

    font add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,17 @@
 // src/pages/_app.tsx
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
-// import { Inter as Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 
-// const inter = Inter({
-//   subsets: ["latin"],
-//   weight: ["400", "500", "700", "900"],
-//   variable: "--font-inter", // sets the font in a CSS variable
-// });
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+  variable: "--font-inter", // sets the font in a CSS variable
+});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main className={` antialiased`}>
+    <main className={` ${inter.className} antialiased`}>
       <Component {...pageProps} />
     </main>
   );
