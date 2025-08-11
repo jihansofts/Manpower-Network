d29fce8af75a586dbbe4bd0bfa67d9c571991283
jihansoft <jihanuddinsoft@gmail.com>
2025-08-11 14:37:44 +0600

    update font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
 // src/pages/_app.tsx
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
-import { Inter_Tight } from "next/font/google";
+import { Inter } from "next/font/google";
 
-const inter = Inter_Tight({
+const inter = Inter({
   subsets: ["latin"],
   weight: ["400", "500", "700", "900"],
   variable: "--font-inter", // sets the font in a CSS variable
