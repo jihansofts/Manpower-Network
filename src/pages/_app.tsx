c9b4ab788eefe4bed88bc88fa051bed67c5d1fe3
jihansoft <jihanuddinsoft@gmail.com>
2025-08-11 11:50:31 +0600

    update font errpr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,10 @@
 // pages/_app.tsx
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Inter } from "next/font/google";
-
-// Load the Inter font
-const inter = Inter({
-  subsets: ["latin"],
-  weight: ["400", "500", "700", "900"],
-  variable: "--font-inter",
-});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={inter.className}>
+    <main>
       <Component {...pageProps} />
     </main>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { Inter } from "next/font/google";
+
+// Load the Inter font
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+  variable: "--font-inter",
+});
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="antialiased">
+      <body className={` ${inter.variable} antialiased`}>
         <Main />
         <NextScript />
       </body>
